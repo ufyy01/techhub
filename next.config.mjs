d883849e26df97d406d5de9a9576a3793768b73e
ufy <ufuomaitwa@gmail.com>
2024-09-23 22:46:21 +0100

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/localhost:3000/api/v1/hub/:path*',
-        destination: `http://localhost:5000/api/v1/hub/:path*`,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
